981ae275d6680216467faba4f9430318550cfc7c
Alexandr Azizyan <alex.r.azizian@gmail.com>
2016-06-09 21:10:30 +0400

    Add PingService

---
+++ src/main/java/ge/edu/freeuni/sdp/iot/room/climate/regulator/core/PingService.java
@@ -0,0 +1,14 @@
+package ge.edu.freeuni.sdp.iot.room.climate.regulator.core;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("ping")
+public class PingService {
+
+    @GET
+    public Response ping() {
+        return Response.ok().build();
+    }
+}
